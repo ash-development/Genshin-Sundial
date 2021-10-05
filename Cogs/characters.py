c7acd4f94a4c3ee3a65f5d06ea6ed5519952462b
Chtholly <91207650+Chtholly2000@users.noreply.github.com>
2021-10-05 18:39:13 +0000

    Improved chr command

--- Cogs/characters.py
+++ Cogs/characters.py
@@ -1,12 +1,16 @@
+
+#Imports
 import discord
-from discord.ext import commands
 import requests
 import DiscordUtils
+
+from discord.ext import commands
+
 class Characters(commands.Cog):
   def __init__(self,bot):
     self.bot=bot
   
-  @commands.command(aliases=['c', 'chara'])
+  @commands.command(aliases=['chr'])
   async def character (self, ctx, *, character: str):
     character = character.replace("'", "-")
     character = character.replace(" ", "-")
@@ -15,7 +19,7 @@ class Characters(commands.Cog):
     json_stats = stats.json()
     stars = ''
     for i in range(json_stats['rarity']):
-      stars += ' ⭐'
+      stars += '⭐'
     name = json_stats["name"]
     desc = json_stats["description"]
     rarity = stars
@@ -24,41 +28,38 @@ class Characters(commands.Cog):
     nation = json_stats["nation"]
     affiliation = json_stats["affiliation"]
     constellation = json_stats["constellation"]
-    birthday = json_stats["birthday"]
+    birthday = json_stats["birthday"].split('-')
+    Birthday = birthday[1] + '/' + birthday[2]
     cons = json_stats["constellations"]
     talent = json_stats["skillTalents"]
     passive = json_stats["passiveTalents"]
-    
-    embed1 = discord.Embed(title=name, description=desc, color=discord.Color.orange())
     element = json_stats["vision"].lower()
+    
+    embed1 = discord.Embed(title=f'__{name}__', description=desc, color=discord.Color.orange())
     embed1.set_thumbnail(url=f"https://api.genshin.dev/elements/{element}/icon")
-    embed1.add_field(name='Rarity', value=rarity, inline=True)
-    embed1.add_field(name='Vision', value=vision, inline=True)
-    embed1.add_field(name='Weapon', value=weapon, inline=True)
-    embed1.add_field(name='Nation', value=nation, inline=True)
-    embed1.add_field(name='Affiliation', value=affiliation, inline=True)
-    embed1.add_field(name='Constellation', value=constellation, inline=True)
-    embed1.add_field(name='Birthday', value=f"(yyyy-mm-dd) {birthday}", inline=True)
+    embed1.add_field(name='__Rarity__', value=f'{rarity}', inline=True)
+    embed1.add_field(name='__Vision__', value=vision, inline=True)
+    embed1.add_field(name='__Weapon__', value=weapon, inline=True)
+    embed1.add_field(name='__Nation__', value=nation, inline=True)
+    embed1.add_field(name='__Affiliation__', value=affiliation, inline=True)
+    embed1.add_field(name='__Constellation__', value=constellation, inline=True)
+    embed1.add_field(name='__Birthday__', value=Birthday, inline=True)
     embed1.set_image(url=f"https://api.genshin.dev/characters/{character}/portrait")
-    embed1.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
     
     embed2 = discord.Embed(title=name, description=f"{desc}\n**Constellation**", color=discord.Color.orange())
     embed2.set_thumbnail(url=f"https://api.genshin.dev/characters/{character}/icon")
     for constel in cons:
-      embed2.add_field(name(="‎\n‎", value="```CSS\n[{}]\n{}```\n```{}```\n".format(constel['name'],constel['unlock'],constel['description']), inline=False)
-    embed2.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
+      embed2.add_field(name="‎\n‎", value="```CSS\n[{}]\n{}```\n```{}```\n".format(constel['name'],constel['unlock'],constel['description']), inline=False)
     
     embed3 = discord.Embed(title=name, description=f"{desc}\n**Talents**", color=discord.Color.orange())
     embed3.set_thumbnail(url=f"https://api.genshin.dev/characters/{character}/icon")
     for skill in talent:
       embed3.add_field(name="‎\n‎", value="```FIX\n[{}]\n{}```\n```{}```\n".format(skill['name'],skill['unlock'],skill['description']), inline=False)
-    embed3.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
     
     embed4 = discord.Embed(title=name, description=f"{desc}\n**Passive**", color=discord.Color.orange())
     embed4.set_thumbnail(url=f"https://api.genshin.dev/characters/{character}/icon")
     for pasif in passive:
       embed4.add_field(name="‎\n‎", value="```FIX\n[{}]\n{}```\n```{}```\n".format(pasif['name'],pasif['unlock'],pasif['description']), inline=False)
-    embed4.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
     
     paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, remove_reactions=True)
     paginator.add_reaction('⏪', "back")
